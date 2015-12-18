e694b1c210745ea834bcd8eb2d6e5fc39c7acff8
Marcel Dierkes <marcel.dierkes@gmail.com>
2015-12-18 13:04:46 +0100

    Determined the general preferences layout.

---
+++ KeepingYouAwake/KYAGeneralPreferencesViewController.m
@@ -0,0 +1,22 @@
+//
+//  KYAGeneralPreferencesViewController.m
+//  KeepingYouAwake
+//
+//  Created by Marcel Dierkes on 18.12.15.
+//  Copyright © 2015 Marcel Dierkes. All rights reserved.
+//
+
+#import "KYAGeneralPreferencesViewController.h"
+
+@interface KYAGeneralPreferencesViewController ()
+
+@end
+
+@implementation KYAGeneralPreferencesViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+@end
