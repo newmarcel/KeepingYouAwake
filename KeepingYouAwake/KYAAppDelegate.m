dc1a7043c48f7960f8c75121484b48a6c4f8ad8e
Marcel Dierkes <marcel@dierkes.co>
2018-12-20 10:20:53 +0100

    Deleted all traces of classic Sparkle

--- KeepingYouAwake/KYAAppDelegate.m
+++ KeepingYouAwake/KYAAppDelegate.m
@@ -7,11 +7,10 @@
 //
 
 #import "KYAAppDelegate.h"
-#import <Sparkle/Sparkle.h>
 #import "NSUserDefaults+Keys.h"
 #import "KYAPreferencesWindowController.h"
 
-@interface KYAAppDelegate () <NSWindowDelegate, SUUpdaterDelegate>
+@interface KYAAppDelegate () <NSWindowDelegate/*, SUUpdaterDelegate*/>
 @property (nonatomic, nullable) KYAPreferencesWindowController *preferencesWindowController;
 @end
 
@@ -43,21 +42,21 @@
     self.preferencesWindowController = nil;
 }
 
-#pragma mark - Updater Delegate
-
-- (NSString *)feedURLStringForUpdater:(SUUpdater *)updater
-{
-    NSString *feedURLString = NSBundle.mainBundle.infoDictionary[@"SUFeedURL"];
-    NSAssert(feedURLString != nil, @"A feed URL should be set in Info.plist");
-    
-    if([NSUserDefaults.standardUserDefaults kya_arePreReleaseUpdatesEnabled])
-    {
-        NSString *lastComponent = feedURLString.lastPathComponent;
-        NSString *baseURLString = feedURLString.stringByDeletingLastPathComponent;
-        return [NSString stringWithFormat:@"%@/prerelease-%@", baseURLString, lastComponent];
-    }
-    
-    return feedURLString;
-}
+//#pragma mark - Updater Delegate
+//
+//- (NSString *)feedURLStringForUpdater:(SUUpdater *)updater
+//{
+//    NSString *feedURLString = NSBundle.mainBundle.infoDictionary[@"SUFeedURL"];
+//    NSAssert(feedURLString != nil, @"A feed URL should be set in Info.plist");
+//
+//    if([NSUserDefaults.standardUserDefaults kya_arePreReleaseUpdatesEnabled])
+//    {
+//        NSString *lastComponent = feedURLString.lastPathComponent;
+//        NSString *baseURLString = feedURLString.stringByDeletingLastPathComponent;
+//        return [NSString stringWithFormat:@"%@/prerelease-%@", baseURLString, lastComponent];
+//    }
+//
+//    return feedURLString;
+//}
 
 @end
