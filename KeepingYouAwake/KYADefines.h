d250f61292515753bc0202f1156dc62ca73cf17b
Marcel Dierkes <marcel@dierkes.co>
2021-03-28 11:26:56 +0200

    Added new Auto and AutoVar defines that replace the KYA_* prefixed versions

--- KeepingYouAwake/KYADefines.h
+++ KeepingYouAwake/KYADefines.h
@@ -9,6 +9,10 @@
 #ifndef KYA_DEFINES_H
 #define KYA_DEFINES_H
 
+#define Auto const __auto_type
+#define AutoVar __auto_type
+
+// These three are considered deprecated:
 #define KYA_AUTO __auto_type const
 #define KYA_AUTO_VAR __auto_type
 #define KYA_WEAK __weak __auto_type
