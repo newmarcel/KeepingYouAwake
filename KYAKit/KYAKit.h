5a823548b6931a4c30a0fa0f01ba838c36c119e4
Marcel Dierkes <marcel@dierkes.co>
2021-03-28 11:23:43 +0200

    Added the KYAKit framework

---
+++ KYAKit/KYAKit.h
@@ -0,0 +1,17 @@
+//
+//  KYAKit.h
+//  KYAKit
+//
+//  Created by Marcel Dierkes on 26.02.21.
+//  Copyright © 2021 Marcel Dierkes. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+/// Project version number for KYAKit.
+FOUNDATION_EXPORT double KYAKitVersionNumber;
+
+/// Project version string for KYAKit.
+FOUNDATION_EXPORT const unsigned char KYAKitVersionString[];
+
+// #import <KYAKit/PublicHeader.h>
