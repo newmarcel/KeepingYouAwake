6f24957f536c206be7c64e78dbdc56bbd526b4ef
Filips Urņežus <filips_urnezus@inbox.lv>
2025-08-16 23:19:44 +0300

    Added Latvian localization

--- Localizations/l10n-config.sh
+++ Localizations/l10n-config.sh
@@ -7,4 +7,4 @@ PROJECT_FILE_PATH="${BASE_DIR}/../${PROJECT_NAME}.xcodeproj"
 WORKSPACE_FILE_PATH="${BASE_DIR}/../${PROJECT_NAME}.xcworkspace"
 TARGET_DIR="${BASE_DIR}/${PROJECT_NAME}"
 
-TRANSLATIONS=("da" "de" "el" "es" "fi" "fr" "hi" "id" "it" "ja" "ko" "nl" "pl" "pt" "ru" "sk" "tr" "uk" "vi" "zh-Hant" "zh")
+TRANSLATIONS=("da" "de" "el" "es" "fi" "fr" "hi" "id" "it" "ja" "ko" "lv" "nl" "pl" "pt" "ru" "sk" "tr" "uk" "vi" "zh-Hant" "zh")
